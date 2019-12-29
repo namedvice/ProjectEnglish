7821b71c8e52463fe9163b77c3018253e956595b
namedvice <namedvice@gmail.com>
2019-12-29 20:43:07 +0300

    Dynamite Throwing, Enemies handling

---
+++ ProjEng/Intermediate/Source/ProjEng.Build.cs
@@ -0,0 +1,12 @@
+using UnrealBuildTool;
+
+public class ProjEng : ModuleRules
+{
+	public ProjEng(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PrivateDependencyModuleNames.Add("Core");
+		PrivateDependencyModuleNames.Add("Core");
+	}
+}
