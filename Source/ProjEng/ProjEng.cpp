dbcf146219189ca5e9a2e86dfd804e438c47b666
namedvice <namedvice@gmail.com>
2021-06-23 12:09:43 +0300

    Package problem solved

---
+++ Source/ProjEng/ProjEng.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjEng.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjEng, "ProjEng" );
